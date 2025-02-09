aba7fbd9f9b6bff9bb7bf1b2851f2537702805a3
bappelberg <benjamin.w.appelberg@gmail.com>
2025-02-09 16:17:36 +0100

    Initial Commit

---
+++ MusicHive/MusicHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicHiveApp.swift
+//  MusicHive
+//
+//  Created by bappelberg on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct MusicHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
